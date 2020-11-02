c2b3a6f2000efc94472aa357e596e635a5544a18
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-11-02 23:18:12 +0530

    configure bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Invoice Application</h1>
+        <Button color="danger">Generate Invoice!</Button>
       </header>
     </div>
   );
